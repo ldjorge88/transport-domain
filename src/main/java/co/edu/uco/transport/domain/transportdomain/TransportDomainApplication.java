b6dd1435236455e7029b7686f2ef1f35bb8aa3ff
ldjorge1988@gmail.com <ldjorge1988@gmail.com>
2022-05-11 22:33:47 -0500

    creacion capa de dominio

---
+++ src/main/java/co/edu/uco/transport/domain/transportdomain/TransportDomainApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uco.transport.domain.transportdomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportDomainApplication.class, args);
+	}
+
+}
